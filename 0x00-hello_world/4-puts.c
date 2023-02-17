b96583afeff36ab42a6e455c2dad0e0193477df8
ebubekin <ebubekin@gmail.com>
2023-02-16 20:40:01 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#!/bin/bash
-include <stdio.h>
+#include <stdio.h>
 
-int main(void){
-	put("Programming is like building a multilingual puzzle")
-		return(0)
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
